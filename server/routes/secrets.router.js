f51d3a3eb79333d92fabdaffb174f3c258b0cdb4
mnpkoerner <mnpkoerner@gmail.com>
2021-02-02 15:25:19 -0600

    secrets.router logic correctly showing correct secrets to users with correct clearance

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -7,14 +7,30 @@ const {rejectUnauthenticated} = require('../modules/authentication-middleware')
 router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
-
-  pool
+  if (req.user.clearance_level >= 13) {
+    pool
     .query(`SELECT * FROM "secret";`)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
       res.sendStatus(500);
     });
-});
+  } else if (req.user.clearance_level >= 6){
+    pool.query(`SELECT * FROM "secret" WHERE "secrecy_level" <= 6;`)
+    .then((results) => res.send(results.rows))
+    .catch((error) => {
+      console.log('Error making SELECT for secrets:', error);
+      res.sendStatus(500);
+    });
+  } else {
+
+  pool
+    pool.query(`SELECT * FROM "secret" WHERE "secrecy_level" <= 3;`)
+    .then((results) => res.send(results.rows))
+    .catch((error) => {
+      console.log('Error making SELECT for secrets:', error);
+      res.sendStatus(500);
+    });
+}})
 
 module.exports = router;
